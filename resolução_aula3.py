e5f60c226b89ef51df9e979758d7922a0d95a388
eliel1995 <168238322+eliel1995@users.noreply.github.com>
2024-05-03 19:21:27 -0300

    Add files via upload

---
+++ resolução_aula3.py
@@ -0,0 +1,70 @@
+# n = 10
+# n2 = 20
+# divisao = n ** n2
+# print(divisao)
+
+
+# letra1 = True
+# letra2 = False
+# compara = letra1 != letra2
+# print(compara)
+
+
+# atribuição  (=)
+# soma (+)
+# subtração (-)
+# multiplicação (*)
+# divisão (/)
+# divisão (//)
+# exponenciação **
+# Maior > 
+# Menor <
+# Maior e igual >=
+# Menor e igual <=
+# Diferente !=
+# igual == 
+
+# 1 Crie um sistema que some 2  variáveis e mostre no Terminal
+
+numero1 = 20
+numero2 = 30
+soma  =  numero1 + numero2
+print(soma)
+
+
+# 2 Crie um sistema que eleve 10 ao cubo
+
+# numero = 10
+# elevado =  10**3
+# print(elevado)
+
+# # 3 Crie um sistema que divida 150 por 3
+
+# number  =  150
+# div  =  number / 3
+# print(div)
+
+# # 4 Crie um sistema que verifique se 135 >= 135
+
+# compara  =  135 >=135
+# print(compara)
+
+
+
+
+# DESAFIO:
+
+# n1 =  int(input('>>'))
+# n2 =  int(input('>>'))
+
+# soma  =  n1 +  n2
+# divisao  =  n1/n2
+# mult =  n1 * n2
+# sub =  n1 - n2
+
+# print('Soma ', soma)
+# print('Divisão ', divisao)
+# print('Multiplicação ', mult)
+# print('Subtração ', sub)
+
+
